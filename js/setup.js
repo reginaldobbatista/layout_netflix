9e5f7693b72129174c0097e32d66c18f12080f9f
reginaldobbatista <reginaldobbatista@gmail.com>
2021-12-12 22:48:49 -0400

    Projeto inicial

---
+++ js/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 12,
+  nav: false,
+  responsive: {
+    0: {
+      items: 4
+    },
+    600: {
+      items: 5
+    },
+    1000: {
+      items: 7
+    }
+  }
+})
